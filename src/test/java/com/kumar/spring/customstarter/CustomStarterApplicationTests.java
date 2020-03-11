776b0b795f87f977942932c9f3754ba4446f8018
kumar vemulapally <30990367+cvemulapally@users.noreply.github.com>
2020-03-10 23:23:53 -0500

    first commit

---
+++ src/test/java/com/kumar/spring/customstarter/CustomStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumar.spring.customstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
